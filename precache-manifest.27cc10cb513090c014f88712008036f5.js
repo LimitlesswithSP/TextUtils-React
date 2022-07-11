9ef3c37be3cf9ac168d2159e276bd3f976459bf0
Shubham Paul <shubham.paul2002@gmail.com>
2022-07-11 12:26:27 +0530

    Updates

---
+++ precache-manifest.27cc10cb513090c014f88712008036f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "21b4b87ddcffb72c4ac2",
+    "url": "/TextUtils-React/static/css/main.f4a8ebb4.chunk.css"
+  },
+  {
+    "revision": "21b4b87ddcffb72c4ac2",
+    "url": "/TextUtils-React/static/js/main.21b4b87d.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/TextUtils-React/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/TextUtils-React/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "54d4ef26c5198954da45",
+    "url": "/TextUtils-React/static/js/runtime~main.54d4ef26.js"
+  },
+  {
+    "revision": "7ed6ff856417eff8b7899d34272e888d",
+    "url": "/TextUtils-React/index.html"
+  }
+];
